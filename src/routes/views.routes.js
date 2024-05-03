ea2fe2a742cb839acf6ab2ad2ab77483ff91cfd7
julio chiappa <juliochiappa43@gmail.com>
2024-05-03 20:44:31 -0300

    Modificación view.routes.js

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,5 +1,8 @@
 import {Router} from 'express';
 import {data, product} from '../data.js';
+import fs from 'fs';
+import path from 'path';
+import config from '../config.js';
 
 const viewRouter = Router();
 
@@ -15,14 +18,39 @@ viewRouter.get('/users', (req, res) => {
 });
 
 viewRouter.get('/home', (req, res) => {
-    const productList = {products: product}
-    res.render('home', productList);
+    const productsFilePath = path.join(config.DIRNAME, './files/products.json');
+    fs.readFile(productsFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error al leer el archivo de productos:', err);
+            return res.status(500).send('Error interno del servidor');
+        }
+        try {
+            const productList = JSON.parse(data);
+            res.render('home', { products: productList });
+        } catch (parseError) {
+            console.error('Error al analizar el archivo JSON de productos:', parseError);
+            return res.status(500).send('Error interno del servidor');
+        }
+    });
 });
 
 viewRouter.get('/realtime_products', (req, res) => {
-    const proList = { products: product };
-    res.render('realtime_products', proList);
-});
+    const productsFilePath = path.join(config.DIRNAME, './files/products.json');
+    fs.readFile(productsFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error al leer el archivo de productos:', err);
+            return res.status(500).send('Error interno del servidor');
+        }
+        try {
+            const productList = JSON.parse(data);
+            res.render('realtime_products', { products: productList });
+        } catch (parseError) {
+            console.error('Error al analizar el archivo JSON de productos:', parseError);
+            return res.status(500).send('Error interno del servidor');
+        }
+    });
 
+
+});
 
 export default viewRouter;
